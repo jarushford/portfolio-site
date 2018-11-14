6c1b07c58f2d44d38cdd9455faebf9de29e162c3
Jamie Rushford <jar.rushford@gmail.com>
2018-11-13 21:52:12 -0500

    Set Up Menu and Hamburger Toggle

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,12 @@
 $(window).scroll(() => {
   let distanceFromTop = $(window).scrollTop() - $('header').offset().top;
-    if (distanceFromTop > 75) {
+    if (distanceFromTop > 20) {
       $('header').addClass(`header-animation`);
     } else {
       $('header').removeClass(`header-animation`);
     }
+});
+
+$('.hamburger').on('click', () => {
+  $('header').toggleClass('menu-rotate');
 });
