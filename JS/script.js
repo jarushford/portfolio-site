e52cb73942d5b22236be31f1ec5076a9a87a4d8d
Jamie Rushford <jar.rushford@gmail.com>
2018-11-14 18:31:44 -0500

    Add Projects and Contact Grids

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,27 @@
 $(window).scroll(() => {
+  checkHeader();
+  checkProjects();
+});
+
+function checkHeader() {
   let distanceFromTop = $(window).scrollTop() - $('header').offset().top;
     if (distanceFromTop > 20) {
       $('header').addClass(`header-animation`);
     } else {
       $('header').removeClass(`header-animation`);
-      if ($('header').hasClass('menu-rotate')) {
-        $('header').removeClass('menu-rotate');
-      }
+      $('header').removeClass('menu-rotate');
     }
-});
+}
+
+function checkProjects() {
+  let distanceFromTop = $(window).scrollTop() - $('.projects').offset().top;
+  if (distanceFromTop > -231) {
+    $('header').addClass('project-color');
+  } else {
+    $('header').removeClass('project-color');
+  }
+}
+
 
 $('.hamburger').on('click', () => {
   $('header').addClass('menu-rotate');
@@ -19,9 +32,11 @@ $('.hamburger-exit').on('click', () => {
 });
 
 $('.fa-angle-down').on('click', () => {
+  if ($(window).scrollTop() === 0) {
     $([document.documentElement, document.body]).animate({
         scrollTop: $('.about').offset().top - 100
     }, 700);
+  }
 });
 
 $('.fa-chevron-circle-up').on('click', () => {
@@ -29,3 +44,22 @@ $('.fa-chevron-circle-up').on('click', () => {
         scrollTop: $('body').offset().top
     }, 700);
 });
+
+$('.projects-text-link').on('click', () => {
+  $([document.documentElement, document.body]).animate({
+        scrollTop: $('.projects').offset().top - 230
+    }, 900);
+});
+
+$('.projects-link').on('click', () => {
+  $([document.documentElement, document.body]).animate({
+        scrollTop: $('.projects').offset().top - 230
+    }, 900);
+});
+
+$('.about-link').on('click', () => {
+  $([document.documentElement, document.body]).animate({
+        scrollTop: $('.about').offset().top - 185
+    }, 900);
+});
+
